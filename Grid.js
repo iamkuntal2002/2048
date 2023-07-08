c89ad977b42a8920512ccd76dc0ef9f20add05e4
kuntal <iamkuntal2002@gmail.com>
2023-07-09 02:21:03 +0530

    9/07/2023

---
+++ Grid.js
@@ -0,0 +1,130 @@
+//making the css variables
+const GRID_SIZE = 4 //we can modify it later
+const CELL_SIZE = 20
+const CELL_GAP = 2
+
+export default class Grid {
+  #cells    //private variables/object
+
+  constructor(gridElement) {
+    gridElement.style.setProperty("--grid-size", GRID_SIZE)
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
+    //we will map through all the cell elements and make it a cell object
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,  //x
+        Math.floor(index / GRID_SIZE)   //y
+      )
+    })
+  }
+
+  get cells() {
+    return this.#cells
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || []
+      cellGrid[cell.y][cell.x] = cell
+      return cellGrid
+    }, [])
+  }
+
+  //get all the cells where 
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || []
+      cellGrid[cell.x][cell.y] = cell
+      return cellGrid
+    }, [])
+  }
+
+  get #emptyCells() {
+    return this.#cells.filter(cell => cell.tile == null)
+  }
+
+  //get any random cell
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+    return this.#emptyCells[randomIndex]
+  }
+}
+
+//creating cell elements by creating gridsize * gridsize
+//we can make out custom grid style based on our requirements
+function createCellElements(gridElement) {
+  const cells = []
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div")
+    cell.classList.add("cell")
+    cells.push(cell)
+    gridElement.append(cell)
+  }
+  return cells
+}
+
+
+//cell class having property for each cell
+class Cell {
+  //cell private variables/objects
+  #cellElement
+  #x
+  #y
+  #tile
+  #mergeTile
+
+  constructor(cellElement, x, y) {
+    this.#cellElement = cellElement
+    this.#x = x
+    this.#y = y
+  }
+
+  get x() {
+    return this.#x
+  }
+
+  get y() {
+    return this.#y
+  }
+
+  get tile() {
+    return this.#tile
+  }
+
+  set tile(value) {
+    this.#tile = value
+    if (value == null) return
+    this.#tile.x = this.#x
+    this.#tile.y = this.#y
+  }
+
+  get mergeTile() {
+    return this.#mergeTile
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value
+    if (value == null) return
+    this.#mergeTile.x = this.#x
+    this.#mergeTile.y = this.#y
+  }
+
+  //when two tiles are added
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    )
+  }
+
+  //merge two tiles
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return
+    this.tile.value = this.tile.value + this.mergeTile.value
+    this.mergeTile.remove()
+    this.mergeTile = null
+  }
+}
+
---
+++ Tile.js
@@ -0,0 +1,62 @@
+export default class Tile {
+  #tileElement
+  #x
+  #y
+  #value
+
+  //tile values will be either 2 or 4
+  constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
+    this.#tileElement = document.createElement("div")
+    this.#tileElement.classList.add("tile")
+    tileContainer.append(this.#tileElement)
+    this.value = value
+  }
+
+  get value() {
+    return this.#value
+  }
+
+  set value(v) {
+    this.#value = v
+    this.#tileElement.textContent = v
+    const power = Math.log2(v)
+    const backgroundLightness = 100 - power * 9 //formula for increasing the lightness
+    //setting the background color of cells
+    this.#tileElement.style.setProperty(
+      "--background-lightness",
+      `${backgroundLightness}%`
+    )
+    //setting the lightness of every cell, like 2 has light color and as the number increased then the lightness is increased
+    this.#tileElement.style.setProperty(
+      "--text-lightness",
+      `${backgroundLightness <= 50 ? 90 : 10}%`
+    )
+  }
+
+  set x(value) {
+    this.#x = value
+    this.#tileElement.style.setProperty("--x", value)
+  }
+
+  set y(value) {
+    this.#y = value
+    this.#tileElement.style.setProperty("--y", value)
+  }
+
+  remove() {
+    this.#tileElement.remove()
+  }
+
+  //transition effect 
+  waitForTransition(animation = false) {
+    return new Promise(resolve => {
+      this.#tileElement.addEventListener(
+        animation ? "animationend" : "transitionend",
+        resolve,
+        {
+          once: true,
+        }
+      )
+    })
+  }
+}
